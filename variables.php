a56d5b9b2106ed2e1248b0c5e1b75210d0ac7428
PMagatteS <papamagattesady@gmail.com>
2022-11-13 21:27:31 +0000

    Php variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+// In order to declare or use variables in php they must start with a "$" sign followed by the name of the variable , for the rest, they follow the same rules as most programming languages.
+//  Php statements end with a semicolon.
+
+$string = "sequence of characters";
+$integer = 123;
+$float  = 12.3;
+
+// The keyword echo is used to output data on the screen.
+// the dot is a Concatenation operator.
+
+echo "A string is a ". $string . "<br>";
+echo  $integer . " is a integer". "<br>";
+echo "And ". $float . " is a float";
+
+
+?>
